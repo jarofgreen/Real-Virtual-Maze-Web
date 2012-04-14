e571f3d4f521b17d2e2d0574f95d9a4e55f64760
James <james@jarofgreen.co.uk>
2012-04-14 12:58:40 +0100

    click to get point

--- buildMaze.js
+++ buildMaze.js
@@ -1,7 +1,41 @@
 var map, layer;
 
 
+
 function init(){
+
+      OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
+                defaultHandlerOptions: {
+                    'single': true,
+                    'double': false,
+                    'pixelTolerance': 0,
+                    'stopSingle': false,
+                    'stopDouble': false
+                },
+
+                initialize: function(options) {
+                    this.handlerOptions = OpenLayers.Util.extend(
+                        {}, this.defaultHandlerOptions
+                    );
+                    OpenLayers.Control.prototype.initialize.apply(
+                        this, arguments
+                    ); 
+                    this.handler = new OpenLayers.Handler.Click(
+                        this, {
+                            'click': this.trigger
+                        }, this.handlerOptions
+                    );
+                }, 
+
+                trigger: function(e) {
+                    var lonlat = map.getLonLatFromViewPortPx(e.xy);
+                    alert("You clicked near " + lonlat.lat + " N, " +
+                                              + lonlat.lon + " E");
+                }
+
+            });
+
+
     map = new OpenLayers.Map( 'Map');
     layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
     map.addLayer(layer);
@@ -11,6 +45,11 @@ function init(){
             map.getProjectionObject()
         ), 12
     );    
+
+    var click = new OpenLayers.Control.Click();
+    map.addControl(click);
+    click.activate();
+
 }
 
 
