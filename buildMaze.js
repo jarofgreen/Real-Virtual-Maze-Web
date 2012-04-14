dc6a06bf6f5133178f7821dc3b9bbebf04d6834b
James <james@jarofgreen.co.uk>
2012-04-14 15:21:47 +0100

    Draw line on map

--- buildMaze.js
+++ buildMaze.js
@@ -1,40 +1,10 @@
 var map, layer;
 
+var lineLayer;
 
+var control;
 
 function init(){
-
-      OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
-                defaultHandlerOptions: {
-                    'single': true,
-                    'double': false,
-                    'pixelTolerance': 0,
-                    'stopSingle': false,
-                    'stopDouble': false
-                },
-
-                initialize: function(options) {
-                    this.handlerOptions = OpenLayers.Util.extend(
-                        {}, this.defaultHandlerOptions
-                    );
-                    OpenLayers.Control.prototype.initialize.apply(
-                        this, arguments
-                    ); 
-                    this.handler = new OpenLayers.Handler.Click(
-                        this, {
-                            'click': this.trigger
-                        }, this.handlerOptions
-                    );
-                }, 
-
-                trigger: function(e) {
-                    var lonlat = map.getLonLatFromViewPortPx(e.xy);
-                    alert("You clicked near " + lonlat.lat + " N, " +
-                                              + lonlat.lon + " E");
-                }
-
-            });
-
 
     map = new OpenLayers.Map( 'Map');
     layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
@@ -46,9 +16,12 @@ function init(){
         ), 15
     );    
 
-    var click = new OpenLayers.Control.Click();
-    map.addControl(click);
-    click.activate();
+	lineLayer = new OpenLayers.Layer.Vector("Line Layer");
+	map.addLayer(lineLayer);
+
+	control = new OpenLayers.Control.DrawFeature(lineLayer,OpenLayers.Handler.Path);
+	map.addControl(control);
+	control.activate();
 
 }
 
